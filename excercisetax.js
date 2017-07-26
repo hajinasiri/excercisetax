74c3fda3caedf388b0bf5417d0ab5a10cbced69e
hajinasiri <hajinasiri@gmail.com>
2017-07-26 20:03:58 +0000

    it worked

--- excercisetax.js
+++ excercisetax.js
@@ -22,16 +22,46 @@ var companySalesData = [
   }
 ];
 
-// function calculateSalesTax (CSD,STR){
-//   result = {};
-//   for( i = 0; i < CSD.length; i++){
-//     if(!result[CSD.name]){
-//       result[CSD.name].totalSales = 0;
-//       result[CSD.name].totalTaxes = 0;
-//     }
+function calculateSalesTax (CSD,STR){
+  var result = {};
+  for( i = 0; i < CSD.length; i++){
+    if(!result[CSD[i].name]){
+      result[CSD[i].name] = {};
+      var TS = 0;//total sales
+      var TT = 0;//total tax
+      for(j = 0; j < CSD[i].sales.length; j++){
+        TS = TS + CSD[i].sales[j];
+      }
+      result[CSD[i].name].totalSales = TS;
+      TT = TS * STR[CSD[i].province];
+      result[CSD[i].name].totalTaxes = TT;
+    }else{
+      TS = 0;
+      TT = 0;
+      for(j = 0; j < CSD[i].sales.length; j++){
+        TS = TS + CSD[i].sales[j];
+      }
+      result[CSD[i].name].totalSales = result[CSD[i].name].totalSales + TS;
+      TT = TS * STR[CSD[i].province];
+      result[CSD[i].name].totalTaxes = result[CSD[i].name].totalTaxes + TT;
 
-//   }
-//   return result;
-// }
+    }
 
-// console.log(calculateSalesTax(salesTaxRates,companySalesData));
+  }
+  return result;
+}
+
+console.log(calculateSalesTax(companySalesData,salesTaxRates));
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
